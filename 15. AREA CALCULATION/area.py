2d3f5fa625bb1f32aa723f3872ec9927317a3f17
Vamsi Krishna K <113965913+vamsikrishnakusetty555@users.noreply.github.com>
2022-10-01 19:31:46 +0530

    CODE

---
+++ 15. AREA CALCULATION/area.py
@@ -0,0 +1,11 @@
+square=int(input(""))
+rec_len=int(input(""))
+rec_bred=int(input(""))
+circle=float(input(""))
+cir_area=3.14*circle**2
+print(square**2)
+print(rec_len*rec_bred)
+print("%.2f"%cir_area)
+
+
+
