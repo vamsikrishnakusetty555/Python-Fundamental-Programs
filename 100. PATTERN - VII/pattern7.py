3523468985359854569f11dcc802068120227c3d
Vamsi Krishna K <113965913+vamsikrishnakusetty555@users.noreply.github.com>
2022-10-02 18:10:51 +0530

    Delete 100. PATTERN - VII directory

--- 100. PATTERN - VII/pattern7.py
+++
@@ -1,8 +0,0 @@
-n=int(input())
-for i in range(n):
-    for j in range(n):
-        if(i == 0 or i == n - 1 or j == 0 or j ==n - 1):
-            print('*', end = '')
-        else:
-            print('', end = ' ')
-    print()
