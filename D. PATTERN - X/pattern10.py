997f21a0eed2bb4d6ee39c26aa5aa7ae438fb943
Vamsi Krishna K <113965913+vamsikrishnakusetty555@users.noreply.github.com>
2022-10-02 18:33:19 +0530

    CODE

---
+++ D. PATTERN - X/pattern10.py
@@ -0,0 +1,18 @@
+n=int(input())
+
+for i in range(0,n-1):
+    for j in range(i,n):
+        print(" ",end='')
+    for j in range(i):
+        print("*",end='')
+    for j in range(i+1):
+        print("*",end='')
+    print()
+for i in range(n):
+    for j in range(i+1):
+        print(" ",end='')
+    for j in range(i,n-1):
+        print("*",end='')
+    for j in range(i,n):
+        print("*",end='')
+    print()
