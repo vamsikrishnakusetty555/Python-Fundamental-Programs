fd63564f451310949cc0497f3a2d0e13a6984630
Vamsi Krishna K <113965913+vamsikrishnakusetty555@users.noreply.github.com>
2022-10-02 12:39:05 +0530

    CIODE

---
+++ 67. DECIMAL TO BINARY/decimal.py
@@ -0,0 +1,13 @@
+number=int(input())
+list=[]
+def bin(x):
+    if x==0:
+        return 0
+    else:
+        digit=x%2
+        list.append(digit)
+        return bin(x//2)
+bin(number)
+list.reverse()
+for i in list:
+    print(i,end="")
