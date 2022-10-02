7184ba2f8989fdf02ee46b33912a1f0a1be22024
Vamsi Krishna K <113965913+vamsikrishnakusetty555@users.noreply.github.com>
2022-10-02 12:43:19 +0530

    CODE

---
+++ 68. CREATE A LIST/list.py
@@ -0,0 +1,10 @@
+size=int(input())
+list=[]
+for i in range(size):
+    elements=int(input())
+    list.append(elements)
+for i in list:
+    print(i,end=" ")
+'''
+list=[int(x) for x in input("").split()]
+'''
