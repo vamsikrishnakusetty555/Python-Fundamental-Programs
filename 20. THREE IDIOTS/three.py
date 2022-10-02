143df1852dc16da76e1de87f0defb90797f4fd48
Vamsi Krishna K <113965913+vamsikrishnakusetty555@users.noreply.github.com>
2022-10-02 10:11:53 +0530

    CODE

---
+++ 20. THREE IDIOTS/three.py
@@ -0,0 +1,9 @@
+x1=int(input())
+y1=int(input())
+x2=int(input())
+y2=int(input())
+mid_point_x=(x1+x2)/2
+mid_point_y=(y1+y2)/2
+print("Binoy's house is located at (",end="")
+print('%.1f'%mid_point_x,end=",")
+print("%.1f"%mid_point_y,")")
