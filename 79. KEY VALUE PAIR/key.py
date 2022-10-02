41a9789f0ed320e8432bd7eec3fd59733a2f35e7
Vamsi Krishna K <113965913+vamsikrishnakusetty555@users.noreply.github.com>
2022-10-02 17:13:52 +0530

    CODE

---
+++ 79. KEY VALUE PAIR/key.py
@@ -0,0 +1,9 @@
+d={}
+key=input()
+value=input()
+'''d[key]=value'''
+d.update({key:value})
+print('''Enter the key (int) to be added:
+Enter the value for the key to be added:
+Updated dictionary is:''')
+print(d)
