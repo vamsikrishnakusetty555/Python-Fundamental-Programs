e850a1fe1e6783b192fb2584e19e29bb2086216d
Vamsi Krishna K <113965913+vamsikrishnakusetty555@users.noreply.github.com>
2022-10-02 12:44:26 +0530

    CODE

---
+++ 69. LARGEST NUMBER IN A LIST/largest.py
@@ -0,0 +1,16 @@
+size=int(input())
+list=[]
+for i in range(size):
+    elements=int(input())
+    list.append(elements)
+print(max(list))
+'''
+size=int(input())
+list=[]
+for i in range(size):
+    elements=int(input())
+    list.append(elements)
+for i in range(1,len(list)):
+    if list[i]>list[i-1]:
+        g=list[i]
+print(g)'''
