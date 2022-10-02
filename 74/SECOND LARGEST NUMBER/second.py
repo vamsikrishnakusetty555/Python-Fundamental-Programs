c8e66d9c6d8dcf4e5c6a7d28b5267d0982b18a05
Vamsi Krishna K <113965913+vamsikrishnakusetty555@users.noreply.github.com>
2022-10-02 13:06:00 +0530

    CODE

---
+++ 74/SECOND LARGEST NUMBER/second.py
@@ -0,0 +1,17 @@
+'''
+# Without Using Sorted()
+list_sorted=[]
+for i in range(len(list)):
+    _min=min(list)
+    list_sorted.append(_min)
+    list.remove(_min)
+print("SORTED : ",list_sorted)
+print(list_sorted[-2])'''
+
+size=int(input())
+list=[]
+for i in range(size):
+    ele=int(input())
+    list.append(ele)
+sort=sorted(list)
+print(sort[-2])
