9c248e1f7fd6ba510f73a428a9142d6776f090b8
Vamsi Krishna K <113965913+vamsikrishnakusetty555@users.noreply.github.com>
2022-10-02 10:39:11 +0530

    CODE

---
+++ 27. LAB ALLOCATION/lab.py
@@ -0,0 +1,18 @@
+x=int(input())
+y=int(input())
+z=int(input())
+if(x<y and x<z):
+    print("Enter x")
+    print("Enter y")
+    print("Enter z")
+    print("L1 has the minimal seating capacity")
+elif(y<x and y<z):
+    print("Enter x")
+    print("Enter y")
+    print("Enter z")
+    print("L2 has the minimal seating capacity")
+elif(z<x and z<y):
+    print("Enter x")
+    print("Enter y")
+    print("Enter z")
+    print("L3 has the minimal seating capacity")
